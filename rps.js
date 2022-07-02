d3c5bec64dc2b8d04e7ca1cf933c6dfc852f8826
Mahmoud <barghoutmahmoud2@gmail.com>
2022-07-02 13:31:54 +0200

    added a new function to play one round using the computer's random return from the previous function and a choice from the player.

--- rps.js
+++ rps.js
@@ -7,5 +7,69 @@ let selector=(Math.floor(Math.random()*3));
 return results[selector];
  }
 
+//function to play one round between the player and the computer
+
+function playRound(playerSelection,computerSelection)
+{
+playerSelection=playerSelection.toLowerCase();
+let gameResult;
+switch (playerSelection)
+{
+case 'rock':
+    if(computerSelection==='rock')
+    {
+        gameResult='computer chose rock.It is a tie!';
+    }
+    else if(computerSelection==='paper')
+    {
+        gameResult='computer chose paper.You lose!';
+    }
+    else
+    {
+        gameResult='computer chose scissors.You win!!';
+    }
+    
+break;
+
+case 'paper':
+    
+    if(computerSelection==='rock')
+    {
+        gameResult='computer chose rock.You win!!';
+    }
+    else if(computerSelection==='paper')
+    {
+        gameResult='computer chose paper.It is a tie!';
+    }
+    else
+    {
+        gameResult='computer chose scissors.You lose!';
+    }
+    
+break;
+
+case 'scissors':
+
+    if(computerSelection==='rock')
+    {
+        gameResult='computer chose rock.You lose!';
+    }
+    else if(computerSelection==='paper')
+    {
+        gameResult='computer chose paper.You win!!';
+    }
+    else
+    {
+        gameResult='computer chose scissors.It is a tie!';
+    }
+    
+break;
+
+default:
+    gameResult='you have not entered a valid selection.try again';
+    
+}
+return gameResult;
 
 
+}
