c4bd7d130ab71f32dbf5b4a30352192c5a4467df
Mahmoud <barghoutmahmoud2@gmail.com>
2022-07-02 13:11:27 +0200

    created a function to give back random plays from the computer.

---
+++ rps.js
@@ -0,0 +1,11 @@
+
+ //function to provide a random play from the computer:
+ function computerPlays()
+ {
+let results=['rock','paper','scissors'];
+let selector=(Math.floor(Math.random()*3));
+return results[selector];
+ }
+
+
+
