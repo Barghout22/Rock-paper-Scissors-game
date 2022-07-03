d7f3ca3bd6b7752ddd8e930b480ab3fbbf0d8cbe
Mahmoud <barghoutmahmoud2@gmail.com>
2022-07-03 18:15:01 +0200

    Finalized the project. adjusted the style sheet to space the items and style the page and color. created counters for the player and computer results created a result showing line. the counter ends the game after one side reaches 5 points and pressing any button resets the game.

--- rps.js
+++ rps.js
@@ -1,10 +1,14 @@
 
  //function to provide a random play from the computer:
- const container=document.querySelector('container');
- const paragraph=document.querySelector('intro');
- let display=document.createElement('p');
- display='Results go here';
+ const container=document.querySelector('#container');
+ const display=document.createElement('p');
+ display.classList.add('display');
+ display.textContent='';
+ const result=document.createElement('p');
+ result.classList.add('result');
+ result.textContent='';
  container.appendChild(display);
+ container.appendChild(result);
  let playerOverallResult=0;
  let computerOverAllResult=0;
  function computerPlays()
@@ -14,129 +18,147 @@ let selector=(Math.floor(Math.random()*3));
 return results[selector];
  }
 
-//function to play one round between the player and the computer
-
+  //function to play one round between the player and the computer
+  
 function playRound(playerSelection,computerSelection)
-{
-playerSelection=playerSelection.toLowerCase();
-if(playerOverallResult==5)
-{
-    container.remove(display);
-    display=`Game ended! Player wins "${playerOverallResult}"-"${computerOverAllResult}"`;
-    container.appendChild(display);
-    return;
-}
-else if(computerOverAllResult==5)
-{
-container.remove(display);
-display=`Game ended! Computer Wins "${computerOverAllResult}"-"${playerOverallResult}"`;
-container.appendChild(display);
-return;
-}
-switch (playerSelection)
-{
-case 'rock':
-    if(computerSelection==='rock')
-    {
-        console.log('computer chose rock.It is a tie!');
-        return 'tie';
-    }
-    else if(computerSelection==='paper')
-    {
-        console.log('computer chose paper.You lose!');
-        computerOverAllResult++;
-        return 'computerWin';
-    }
-    else
-    {
-        console.log('computer chose scissors.You win!!');
-        playerOverallResult++;
-        return 'playerWin';
-    }
-    
-break;
+  {
+  
+  switch (playerSelection)
+  {
+  case 'rock':
+      if(computerSelection==='rock')
+      {
+        display.textContent='computer chose rock.It is a tie!'; 
+      }
+      else if(computerSelection==='paper')
+      {
+        display.textContent='computer chose paper.You lose!';
+          computerOverAllResult++;
+      }
+      else
+      {
+        display.textContent='computer chose scissors.You win!!';
+          playerOverallResult++;
+      }
+      result.textContent=`player:${playerOverallResult}- Computer: ${computerOverAllResult}`;
+  
+  
+      if(playerOverallResult==5)
+      {
+          display.textContent=`Game ended! Player wins ${playerOverallResult}-${computerOverAllResult}`;
+          result.textContent= 'Press a button to play again!';
+         // container.appendChild(display);
+          computerOverAllResult=0;
+          playerOverallResult=0;
+          return;
+      }
+      else if(computerOverAllResult==5)
+      {
+       
+        display.textContent=`Game ended! Computer Wins:${computerOverAllResult}-${playerOverallResult}`; 
+        result.textContent='Press a button to play again!';
+        //container.appendChild(display);
+        computerOverAllResult=0;
+        playerOverallResult=0;
+        return;  
+      }
+  
+  
+  
+      break;
+  
+  case 'paper':
+      
+      if(computerSelection==='rock')
+      {
+        display.textContent='computer chose rock.You win!!';
+          playerOverallResult++;
+      }
+      else if(computerSelection==='paper')
+      {
+        display.textContent='computer chose paper.It is a tie!';
+      }
+      else
+      {
+        display.textContent='computer chose scissors.You lose!';
+          computerOverAllResult++;
+      }
 
-case 'paper':
-    
-    if(computerSelection==='rock')
-    {
-        console.log('computer chose rock.You win!!');
-        playerOverallResult++;
-        return 'playerWin';
-    }
-    else if(computerSelection==='paper')
-    {
-        console.log('computer chose paper.It is a tie!');
-        return 'tie';
-    }
-    else
-    {
-        console.log('computer chose scissors.You lose!');
-        computerOverAllResult++;
-        return 'computerWin';
-    }
-    
-break;
-
-case 'scissors':
-
-    if(computerSelection==='rock')
-    {
-        console.log('computer chose rock.You lose!');
-        computerOverAllResult++;
-        return 'computerWin';
-    }
-    else if(computerSelection==='paper')
-    {
-        console.log('computer chose paper.You win!!');
-        playerOverallResult++;
-        return 'playerWin';
-    }
-    else
-    {
-        console.log('computer chose scissors.It is a tie!');
-        return 'tie';
-    }
-    
-break;
-
-default:
-   console.log('you have entered an invalid selection.try again');
-    
-}
-
-}
-
-const buttons=document.querySelectorAll('button');
-
-buttons.forEach(button=>button.addEventListener('click',()=>{playRound(button.classList.value,computerPlays())}))
-
-//a function to play a game of five rounds .
-/*function game()
-{
-let playerWins= 0;
-let computerWins= 0;
-let roundResult;
-
-for(let i=0;i<5;i++)
-{
-let playerChoice=prompt('enter your selection:')
-roundResult=playRound(playerChoice,computerPlays());
-if (roundResult==='playerWin')
-{
-    playerWins++;
-}
-else if (roundResult==='computerWin')
-{
-computerWins++;
-}
-
-console.log(`you:${playerWins} - computer:${computerWins}`);
-
-}
+      result.textContent=`player:${playerOverallResult}- Computer: ${computerOverAllResult}`;
 
 
-}
+      if(playerOverallResult==5)
+      {
+          display.textContent=`Game ended! Player wins ${playerOverallResult}-${computerOverAllResult}`;
+          result.textContent= 'Press a button to play again!';
+         // container.appendChild(display);
+          computerOverAllResult=0;
+          playerOverallResult=0;
+          return;
+      }
+      else if(computerOverAllResult==5)
+      {
+       
+        display.textContent=`Game ended! Computer Wins:${computerOverAllResult}-${playerOverallResult}`; 
+        result.textContent='Press a button to play again!';
+        //container.appendChild(display);
+        computerOverAllResult=0;
+        playerOverallResult=0;
+        return;  
+      }
+  break;
+  
+  case 'scissors':
+  
+      if(computerSelection==='rock')
+      {
+          display.textContent='computer chose rock.You lose!';
+          computerOverAllResult++;
+      }
+      else if(computerSelection==='paper')
+      {
+        display.textContent='computer chose paper.You win!!';
+          playerOverallResult++;
+      }
+      else
+      {
+        display.textContent='computer chose scissors.It is a tie!';
+      }
+      result.textContent=`player:${playerOverallResult}- Computer: ${computerOverAllResult}`;
+  
+      if(playerOverallResult==5)
+      {
+          display.textContent=`Game ended! Player wins ${playerOverallResult}-${computerOverAllResult}`;
+          result.textContent= 'Press a button to play again!';
+         // container.appendChild(display);
+          computerOverAllResult=0;
+          playerOverallResult=0;
+          return;
+      }
+      else if(computerOverAllResult==5)
+      {
+       
+        display.textContent=`Game ended! Computer Wins:${computerOverAllResult}-${playerOverallResult}`; 
+        result.textContent='Press a button to play again!';
+        //container.appendChild(display);
+        computerOverAllResult=0;
+        playerOverallResult=0;
+        return;  
+      }
+  
+  
+  
+  
+      break;
+      
+  
+  }
+  
+  }
+ 
+  const buttons=document.querySelectorAll('button');
+  
+  buttons.forEach(button=>button.addEventListener('click',()=>{playRound(button.classList.value,computerPlays())}))
+  
 
-game();
-*/
+  
